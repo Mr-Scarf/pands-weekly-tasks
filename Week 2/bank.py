9fb618e26ee50fe2fd6ccb7a7e9c940a4320b952
Mr-Scarf <dscal07@outlook.com>
2025-02-19 08:45:24 +0000

    week02

---
+++ Week 2/bank.py
@@ -0,0 +1,15 @@
+ # bank.py
+# Prompt the user and read in two money amounts (in cent)
+# cont. Add the two amounts
+# cont. Print out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount 
+# Author: David Scally
+
+#Prompt User for two money amounts
+amount1 = (input("Enter first amount in cents :"))
+amount2 = (input("Enter second amount in cents :"))
+
+# Add two amounts
+
+
+# Display Answer to two decimal places
+
