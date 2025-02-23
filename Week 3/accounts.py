1ec134894bcba69a09a7a2e3894ed21f82818d5e
Mr-Scarf <dscal07@outlook.com>
2025-02-23 14:00:08 +0000

    Week03

---
+++ Week 3/accounts.py
@@ -0,0 +1,9 @@
+# accounts.py
+# Write a python program called accounts.py that reads in a 10 character account number and outputs the account number
+# with only the last 4 digits showing (and the first 6 digits replaced with Xs).
+# Author: David Scally
+
+
+#Currently, is no limit on how many digits a user can enter, needs to be limited to 10.
+
+Account_Number = (input("Please enter a 10 digit account number: "))
