70f7ed00f541f53c259ba007a120c9b20dfb7379
Mr-Scarf <dscal07@outlook.com>
2025-03-02 21:00:10 +0000

    Week05

---
+++ Week 5/weekday.py
@@ -0,0 +1,10 @@
+# weekday.py
+
+# Write a program that outputs whether or not today is a weekday. (The program should be called weekday.py)
+# You will need to search the web to find how you work out what day it is.
+
+# Author: David Scally
+x = 2
+print(x)
+
+
