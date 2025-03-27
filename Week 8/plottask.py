8ed11b49fa6752619ff53606be29e8edd5b90c20
Mr-Scarf <dscal07@outlook.com>
2025-03-27 11:41:35 +0000

    Week8 - Weekly Task - Update Code + Comments (i) '

--- Week 8/plottask.py
+++ Week 8/plottask.py
@@ -4,27 +4,43 @@
 
 #Author: David Scally
 
+#Import libraries
 import matplotlib.pyplot as plt
 import numpy as np
 
 #Inputs from Instruction
-
 Mean = 5
 standard_deviation = 2
 number_of_values = 1000
 
-# Generate values
+# Generate 1000 values
 values = np.random.normal(Mean,standard_deviation,number_of_values)
 
-# Plot histogram, add title, lable x & y axis
+# Plot histogram
+plt.hist(values,bins = 20, alpha = 0.8, color = 'green',label = 'Normal Distribution')
 
-plt.hist(values)
+# Add title + name x& y axis
 plt.title('Week 8 - Plot Task')
 plt.xlabel('Value')
 plt.ylabel('Count')
 
+# Add gridlines + legend
+plt.grid(True)
+plt.legend()
+
 # Show Data
 plt.show()
+
+# plot of the function  h(x)=x3 in the range 0 to 10,
+
+
+
+
+
+# Reference: https://numpy.org/doc/stable/reference/random/generated/numpy.random.normal.html
+# Reference https://campus.datacamp.com/courses/intermediate-python/matplotlib?ex=18
+
+
 
 #min_salary = 20000
 #max_salary = 80000
